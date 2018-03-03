38296c50deab7b0284820057180f0ffdee43ab4c
chronek <chrisjhronek@gmail.com>
2018-03-03 01:59:39 -0600

    Tiling the field

--- jsPixi/test.js
+++ jsPixi/test.js
@@ -1,39 +1,11 @@
-var app = new PIXI.Application(800, 600, {backgroundColor : 0x1099bb});
+var app = new PIXI.Application(500, 600);
+
 document.body.appendChild(app.view);
 
-// create a new Sprite from an image path
-var dot1 = PIXI.Sprite.fromImage('Black_dot.png')
-var dot2 = PIXI.Sprite.fromImage('Black_dot.png')
-var dot3 = PIXI.Sprite.fromImage('Black_dot.png')
+var texture = PIXI.Texture.fromImage("dark_green.png");
+var texture2 = PIXI.Texture.fromImage("light_green.jpg");
 
-// center the sprite's anchor point
-dot1.anchor.set(0.5);
-dot2.anchor.set(0.5);
-dot3.anchor.set(0.5);
+var tile1 = new PIXI.TilingSprite(texture, 500, 100);
+var tile2 = new PIXI.TilingSprite(texture2, 500, 100);
 
-
-// move the sprite to the center of the screen
-dot1.x = app.screen.width / 2 + 10;
-dot1.y = app.screen.height / 2;
-
-app.stage.addChild(dot1);
-
-dot2.x = app.screen.width / 2 + 15;
-dot2.y = app.screen.height / 2;
-
-app.stage.addChild(dot2);
-
-dot3.x = app.screen.width / 2 + 20;
-dot3.y = app.screen.height / 2;
-
-app.stage.addChild(dot3s);
-
-// Listen for animate update
-app.ticker.add(function(delta) {
-    // just for fun, let's rotate mr rabbit a little
-    // delta is 1 if running at 100% performance
-    // creates frame-independent transformation
-    dot1.y += delta;
-    dot2.y += delta;
-    dot3.y += delta;
-});
+app.stage.addChild(tile1, tile2);
