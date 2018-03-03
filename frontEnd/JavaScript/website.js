4823145f8cdd1fd93445a3f18df8858831bd7735
TheBTG <garrardbrandon@gmail.com>
2018-03-03 09:59:39 -0600

    gone backwards trying to scrape the text

--- frontEnd/JavaScript/website.js
+++ frontEnd/JavaScript/website.js
@@ -22,3 +22,14 @@ $(document).ready(function(){
     $("#video-tog2").toggle();
   })
 })
+
+var url = "http://allorigins.me/get?url=" + encodeURIComponent("http://www.nfl.com/rulebook/beginnersguidetofootball");
+$.get(url, function(response){
+
+  console.log(response);
+})
+
+var myText = response.match(/<div id="nflbasics">\*<\/div>/);
+function test(){
+  document.write(myText);
+}
